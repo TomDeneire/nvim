9c14e83d625955b5793e3348acea522936295531
Tom Deneire <tomdeneire@gmail.com>
2023-03-10 10:56:09 +0100

    new setup notify-diagnostics

--- lua/custom/plugins/notify-diagnostics.lua
+++ lua/custom/plugins/notify-diagnostics.lua
@@ -6,7 +6,10 @@ return {
     'tomdeneire/notify-diagnostics.nvim',
     event = { "BufReadPost", "BufNewFile" },
     dependencies = { 'rcarriga/nvim-notify' },
+    branch = "dev",
     config = function()
         require("notify-diagnostics").setup({ exclude_codes = { E501 = true } })
+        local max_width = math.floor(vim.o.columns * 0.25)
+        require("notify").setup({ max_width = max_width })
     end
 }
