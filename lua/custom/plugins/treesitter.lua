1679f49ea6812f918b23fd155c82d91a05abfc57
Tom Deneire <tomdeneire@gmail.com>
2023-04-21 06:33:36 +0200

    regex_highlighting off in treesitter!

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -41,7 +41,6 @@ return
             highlight = {
                 -- `false` will disable the whole extension
                 enable = true,
-
                 -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
                 -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
                 -- the name of the parser)
@@ -60,8 +59,7 @@ return
                 -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
                 -- Using this option may slow down your editor, and you may see some duplicate highlights.
                 -- Instead of true it can also be a list of languages
-                -- (do not disable this!)
-                additional_vim_regex_highlighting = true,
+                additional_vim_regex_highlighting = false,
             },
         }
     end
