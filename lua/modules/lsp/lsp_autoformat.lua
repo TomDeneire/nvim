2ec1cb2c508804ef147bc1fa953dfee21950d29c
Tom Deneire <tomdeneire@gmail.com>
2023-06-28 16:25:17 +0200

    move autoformat to lsp module

--- lua/modules/lsp/lsp_autoformat.lua
+++ lua/modules/lsp/lsp_autoformat.lua
@@ -1,7 +1,6 @@
--- autoformat.lua
---
 -- Use your language server to automatically format your code on save.
 -- Adds additional commands as well to manage the behavior
+-- Be sure to configure this AFTER general lsp has been set up!
 
 return {
     'neovim/nvim-lspconfig',
