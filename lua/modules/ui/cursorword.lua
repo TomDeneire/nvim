fb5255fd1a7c0b3aedea93de0e30a5d617e19ce3
Tom Deneire <tomdeneire@gmail.com>
2023-07-03 16:10:05 +0200

    highlight cursorword

---
+++ lua/modules/ui/cursorword.lua
@@ -0,0 +1,18 @@
+return
+{
+    'yamatsum/nvim-cursorline',
+    config = function()
+        require('nvim-cursorline').setup {
+            cursorline = {
+                enable = false,
+                timeout = 1000,
+                number = false,
+            },
+            cursorword = {
+                enable = true,
+                min_length = 3,
+                hl = { underline = true },
+            }
+        }
+    end
+}
