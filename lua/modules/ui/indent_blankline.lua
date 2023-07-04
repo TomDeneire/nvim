382d6ba0acc271bc1e3903324c37830d0d3143cf
Tom Deneire <tomdeneire@gmail.com>
2023-07-04 07:39:01 +0200

    undo current_context_start

--- lua/modules/ui/indent_blankline.lua
+++ lua/modules/ui/indent_blankline.lua
@@ -6,7 +6,7 @@ return {
             show_end_of_line = true,
             space_char_blankline = " ",
             show_current_context = true,
-            show_current_context_start = true,
+            show_current_context_start = false,
         }
     end
 }
