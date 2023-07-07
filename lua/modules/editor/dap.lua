8a3bcadf7edbfaa8bbb3c59002560a70b7ea7162
Tom Deneire <tomdeneire@gmail.com>
2023-07-07 14:38:47 +0200

    add python debugger

--- lua/modules/editor/dap.lua
+++ lua/modules/editor/dap.lua
@@ -1,8 +1,69 @@
 return {
-    "leoluz/nvim-dap-go",
-    ft = { "go" },
-    dependencies = { "mfussenegger/nvim-dap", "rcarriga/nvim-dap-ui" },
+    "mfussenegger/nvim-dap",
+    ft = { "go", "python" },
+    dependencies = {
+        "leoluz/nvim-dap-go",
+        "rcarriga/nvim-dap-ui" },
     config = function()
+        local dap = require('dap')
+
+        -- Go
+        require('dap-go').setup {}
+
+        -- Python
+        dap.adapters.python = function(cb, config)
+            if config.request == 'attach' then
+                ---@diagnostic disable-next-line: undefined-field
+                local port = (config.connect or config).port
+                ---@diagnostic disable-next-line: undefined-field
+                local host = (config.connect or config).host or '127.0.0.1'
+                cb({
+                    type = 'server',
+                    port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+                    host = host,
+                    options = {
+                        source_filetype = 'python',
+                    },
+                })
+            else
+                cb({
+                    type = 'executable',
+                    command = 'py3',
+                    args = { '-m', 'debugpy.adapter' },
+                    options = {
+                        source_filetype = 'python',
+                    },
+                })
+            end
+        end
+
+        dap.configurations.python = {
+            {
+                -- The first three options are required by nvim-dap
+                type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+                request = 'launch',
+                name = "Launch file",
+
+                -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+                program = "${file}", -- This configuration will launch the current file if used.
+                pythonPath = function()
+                    -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+                    -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+                    -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+                    local cwd = vim.fn.getcwd()
+                    if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+                        return cwd .. '/venv/bin/python'
+                    elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+                        return cwd .. '/.venv/bin/python'
+                    else
+                        return 'py3'
+                    end
+                end,
+            },
+        }
+
+        -- UI
         require("dapui").setup(
             {
                 controls = {
@@ -81,7 +142,11 @@ return {
                 }
             }
         )
-        require('dap-go').setup {}
+
+        -- Highlight
+        vim.fn.sign_define('DapBreakpoint', { text = '', texthl = '', linehl = '', numhl = '' })
+
+        -- Keymaps
         vim.keymap.set("n", "<leader>db", require("dap").toggle_breakpoint,
             { desc = 'DAP toggle breakpoint' })
         vim.keymap.set("n", "<leader>dc", require("dap").continue,
