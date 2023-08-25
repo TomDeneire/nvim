65f7abe131ccc63a58b8a8e6129449e94cc2e626
Tom Deneire <tomdeneire@gmail.com>
2023-08-25 06:32:59 +0200

    add style lua

--- lua/modules/lsp/null-ls.lua
+++ lua/modules/lsp/null-ls.lua
@@ -1,7 +1,7 @@
 -- null-ls (only used for formatting)
 
 return {
-    'jose-elias-alvarez/null-ls.nvim',
+    "jose-elias-alvarez/null-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
         local null_ls = require("null-ls")
@@ -12,8 +12,8 @@ return {
                 null_ls.builtins.formatting.xmlformat,
                 null_ls.builtins.formatting.djlint,
                 null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.stylua,
+                -- null_ls.builtins.formatting.stylua,
             },
         })
-    end
+    end,
 }
