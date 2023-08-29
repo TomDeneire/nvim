d8e4d2eab33b0d1ab9c2d8cd6dcb681d96f82b6a
Tom Deneire <tomdeneire@gmail.com>
2023-08-29 13:04:48 +0200

    silent command

--- lua/modules/editor/undotree.lua
+++ lua/modules/editor/undotree.lua
@@ -3,7 +3,13 @@ return {
     lazy = true,
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-        { "<leader>u", ":lua require('undotree').toggle()<cr>", silent = true, desc = "Show undotree", mode = "n" } },
+        {
+            "<leader>u",
+            "<cmd>silent :lua require('undotree').toggle()<cr>",
+            silent = true,
+            desc = "Show undotree",
+            mode = "n"
+        } },
     config = function()
         require("undotree").setup()
     end
