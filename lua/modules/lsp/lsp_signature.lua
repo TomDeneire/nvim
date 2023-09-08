bcb1bcb261c7b46b36aaaee82a30eaca3c61682c
Tom Deneire <tomdeneire@gmail.com>
2023-09-08 17:44:35 +0200

    improve lsp_signature style

--- lua/modules/lsp/lsp_signature.lua
+++ lua/modules/lsp/lsp_signature.lua
@@ -19,29 +19,29 @@ local cfg = {
     -- will set to true when fully tested, set to false will use whichever side has more space
     -- this setting will be helpful if you do not want the PUM and floating win overlap
 
-    floating_window_off_x = 1,                    -- adjust float windows x position.
-    floating_window_off_y = 0,                    -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
-    close_timeout = 4000,                         -- close floating window after ms when laster parameter is entered
-    fix_pos = false,                              -- set to true, the floating window will not auto-close until finish all parameters
-    hint_enable = false,                          -- virtual hint enable
-    hint_prefix = "🐼 ",                        -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
+    floating_window_off_x = 1, -- adjust float windows x position.
+    floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
+    close_timeout = 4000, -- close floating window after ms when laster parameter is entered
+    fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
+    hint_enable = false, -- virtual hint enable
+    hint_prefix = "🐼 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
     hint_scheme = "String",
     hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
     handler_opts = {
-        border = "none"                           -- double, rounded, single, shadow, none, or a table of borders
+        border = "single" -- double, rounded, single, shadow, none, or a table of borders
     },
-    always_trigger = false,                       -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
-    auto_close_after = nil,                       -- autoclose signature float win after x sec, disabled if nil.
-    extra_trigger_chars = {},                     -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
-    zindex = 200,                                 -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
-    padding = '',                                 -- character to pad on left and right of signature can be ' ', or '|'  etc
-    transparency = nil,                           -- disabled by default, allow floating win transparent value 1~100
-    shadow_blend = 36,                            -- if you using shadow as border use this set the opacity
-    shadow_guibg = 'Black',                       -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
-    timer_interval = 200,                         -- default timer check interval set to lower value if you want to reduce latency
-    toggle_key = nil,                             -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
-    select_signature_key = nil,                   -- cycle to next signature, e.g. '<M-n>' function overloading
-    move_cursor_key = nil,                        -- imap, use nvim_set_current_win to move cursor between current win and floating
+    always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
+    auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
+    extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+    zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
+    padding = '', -- character to pad on left and right of signature can be ' ', or '|'  etc
+    transparency = nil, -- disabled by default, allow floating win transparent value 1~100
+    shadow_blend = 36, -- if you using shadow as border use this set the opacity
+    shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
+    timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
+    toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+    select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
+    move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
 }
 
 return {
