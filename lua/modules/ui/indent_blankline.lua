9153a4f1c1c20f2b7a02088d03827cacf0acaa67
Tom Deneire <tomdeneire@gmail.com>
2023-09-28 09:04:46 +0200

    upgrade to version 3

--- lua/modules/ui/indent_blankline.lua
+++ lua/modules/ui/indent_blankline.lua
@@ -2,7 +2,7 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     event = { "BufReadPost", "BufNewFile" },
     config = function()
-        require("indent_blankline").setup {
+        require("ibl").setup {
             show_end_of_line = true,
             space_char_blankline = " ",
             show_current_context = true,
