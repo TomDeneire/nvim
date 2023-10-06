0296ba5c8fa875f2e10f95abc615c803799d0b77
Tom Deneire <tomdeneire@gmail.com>
2023-10-06 09:14:50 +0200

    no scope marking

--- lua/modules/ui/indent_blankline.lua
+++ lua/modules/ui/indent_blankline.lua
@@ -7,6 +7,7 @@ return {
             space_char_blankline = " ",
             show_current_context = true,
             show_current_context_start = false,
+            scope = { enabled = false },
         }
     end
 }
