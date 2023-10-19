c596c7e498278240b3e566ab64d8fcf0ef86a914
Tom Deneire <tomdeneire@gmail.com>
2023-10-19 15:06:58 +0200

    add lsp timeout

---
+++ lua/modules/lsp/lsp_timeout.lua
@@ -0,0 +1,11 @@
+return {
+    "hinell/lsp-timeout.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
+    init = function()
+        vim.g["lsp-timeout-config"] = {
+            stopTimeout  = 1000 * 60 * 5, -- ms, timeout before stopping all LSP servers
+            startTimeout = 1000 * 10,     -- ms, timeout before restart
+            silent       = true           -- true to suppress notifications
+        }
+    end
+}
