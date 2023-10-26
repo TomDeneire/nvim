97207774638ec9eb31c382eab9a339efe36f8dca
Tom Deneire <tomdeneire@gmail.com>
2023-10-26 14:53:46 +0200

    (fix) breaking changes ibl config

--- lua/modules/ui/indent_blankline.lua
+++ lua/modules/ui/indent_blankline.lua
@@ -3,11 +3,13 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     config = function()
         require("ibl").setup {
-            show_end_of_line = true,
-            space_char_blankline = " ",
-            show_current_context = true,
-            show_current_context_start = false,
-            scope = { enabled = false },
+            indent = { char = "|" },
+            scope = {
+                char = "|",
+                enabled = false,
+                show_start = true,
+                show_end = true
+            },
         }
     end
 }
