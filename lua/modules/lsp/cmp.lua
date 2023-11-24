5454656a133c822e928beb6cbcffa6e37a4355f5
Tom Deneire <tomdeneire@gmail.com>
2023-11-24 16:05:47 +0100

    (chore) better completion settings

--- lua/modules/lsp/cmp.lua
+++ lua/modules/lsp/cmp.lua
@@ -22,7 +22,7 @@ return
         cmp.setup {
             formatting = {
                 format = lspkind.cmp_format({
-                    mode = 'symbol',       -- show only symbol annotations
+                    mode = 'symbol-text',  -- show only symbol annotations
                     maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
                     ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
                     symbol_map = { Codeium = "", },
@@ -32,6 +32,10 @@ return
                         return vim_item
                     end
                 })
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
             },
             snippet = {
                 expand = function(args)
