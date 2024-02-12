d79335af6b3beb4ad35e4eb759732baf03bf2c01
Tom Deneire <tomdeneire@gmail.com>
2024-02-12 15:15:30 +0100

    (feat) fuzzy find in current buffer

--- lua/modules/settings/remap.lua
+++ lua/modules/settings/remap.lua
@@ -17,6 +17,14 @@ vim.keymap.set({ "n", "x", "o" }, "H", "^", { desc = "Jump to start of line" })
 vim.keymap.set({ "n", "x", "o" }, "L", "$", { desc = "Jump to end of line" })
 vim.keymap.set({ "n", "x", "o" }, "J", "<c-i>", { desc = "Jump to start of line" })
 vim.keymap.set({ "n", "x", "o" }, "K", "<c-o>", { desc = "Jump to end of line" })
+vim.keymap.set('n', '<leader>/', function()
+    -- You can pass additional configuration to telescope to change theme, layout, etc.
+    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+        layout_config = { width = 0.7 }
+    })
+end, { desc = '[/] Fuzzily search in current buffer' })
 -- Paste but don't overwrite copy-register
 vim.keymap.set("v", "p", '"_dP')
 -- Make Y behave like C and D
