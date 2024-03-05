bac096a97677c1c03af1db20ca24dc34bade2457
Tom Deneire <tomdeneire@gmail.com>
2024-03-05 08:00:00 +0100

    (chore) better add/change symbol

--- lua/modules/ui/gitsigns.lua
+++ lua/modules/ui/gitsigns.lua
@@ -4,8 +4,8 @@ return {
     config = function()
         require('gitsigns').setup {
             signs                        = {
-                add          = { text = '│' },
-                change       = { text = '│' },
+                add          = { text = '+' },
+                change       = { text = '~' },
                 delete       = { text = '_' },
                 topdelete    = { text = '‾' },
                 changedelete = { text = '~' },
