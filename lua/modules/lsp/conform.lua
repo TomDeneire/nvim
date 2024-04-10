019250a2e161c04e2803f5ae419962d628adfb99
Tom Deneire <tomdeneire@gmail.com>
2024-04-10 13:42:45 +0200

    (feat) enable format toggling

--- lua/modules/lsp/conform.lua
+++ lua/modules/lsp/conform.lua
@@ -25,14 +25,34 @@ return {
 				["handlebars"] = { "prettier" },
 				["xml"] = { "xmlformatter" },
 			},
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				-- Timout of 1000 is better for larger files (than suggested 500)
-				timeout_ms = 1000,
-				lsp_fallback = true,
-			},
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 1000, lsp_fallback = true }
+			end,
 		})
+
 		vim.keymap.set("n", "<leader>c", ":lua require('conform').format({ timeout_ms = 100000 })<cr>",
 			{ desc = "Format with conform with very long timeout" })
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
 	end
 }
