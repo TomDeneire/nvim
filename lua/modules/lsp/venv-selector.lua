775ecb4287114e5ba992b3525ae3c7f40632a324
Tom Deneire <tomdeneire@gmail.com>
2024-07-08 17:54:26 +0200

    (feat): bump venv-selector to new version

--- lua/modules/lsp/venv-selector.lua
+++ lua/modules/lsp/venv-selector.lua
@@ -1,6 +1,7 @@
 return {
     "linux-cultist/venv-selector.nvim",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    branch = "regexp",
     opts = {
         -- Your options go here
         -- name = "venv",
