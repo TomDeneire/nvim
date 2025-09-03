d0cea866221190526e06898e8e3866115ecd15b6
Tom Deneire <tomdeneire@gmail.com>
2025-09-03 16:29:59 +0200

    (feat): better markdown functionality

--- lua/modules/editor/markdown.lua
+++ lua/modules/editor/markdown.lua
@@ -8,6 +8,68 @@ return {
     opts = {},
     config = function()
         require('render-markdown').setup({
+            bullet = {
+                -- Useful context to have when evaluating values.
+                -- | level | how deeply nested the list is, 1-indexed          |
+                -- | index | how far down the item is at that level, 1-indexed |
+                -- | value | text value of the marker node                     |
+
+                -- Turn on / off list bullet rendering
+                enabled = true,
+                -- Additional modes to render list bullets
+                render_modes = false,
+                -- Replaces '-'|'+'|'*' of 'list_item'.
+                -- If the item is a 'checkbox' a conceal is used to hide the bullet instead.
+                -- Output is evaluated depending on the type.
+                -- | function   | `value(context)`                                    |
+                -- | string     | `value`                                             |
+                -- | string[]   | `cycle(value, context.level)`                       |
+                -- | string[][] | `clamp(cycle(value, context.level), context.index)` |
+                icons = { '●', '○', '◆', '◇' },
+                -- Replaces 'n.'|'n)' of 'list_item'.
+                -- Output is evaluated using the same logic as 'icons'.
+                ordered_icons = function(ctx)
+                    local value = vim.trim(ctx.value)
+                    local index = tonumber(value:sub(1, #value - 1))
+                    return ('%d.'):format(index > 1 and index or ctx.index)
+                end,
+                -- Padding to add to the left of bullet point.
+                -- Output is evaluated depending on the type.
+                -- | function | `value(context)` |
+                -- | integer  | `value`          |
+                left_pad = 0,
+                -- Padding to add to the right of bullet point.
+                -- Output is evaluated using the same logic as 'left_pad'.
+                right_pad = 0,
+                -- Highlight for the bullet icon.
+                -- Output is evaluated using the same logic as 'icons'.
+                highlight = 'RenderMarkdownBullet',
+                -- Highlight for item associated with the bullet point.
+                -- Output is evaluated using the same logic as 'icons'.
+                scope_highlight = {},
+            },
+            indent = {
+                -- Mimic org-indent-mode behavior by indenting everything under a heading based on the
+                -- level of the heading. Indenting starts from level 2 headings onward by default.
+
+                -- Turn on / off org-indent-mode.
+                enabled = true,
+                -- Additional modes to render indents.
+                render_modes = false,
+                -- Amount of additional padding added for each heading level.
+                per_level = 2,
+                -- Heading levels <= this value will not be indented.
+                -- Use 0 to begin indenting from the very first level.
+                skip_level = 1,
+                -- Do not indent heading titles, only the body.
+                skip_heading = false,
+                -- Prefix added when indenting, one per level.
+                icon = '▎',
+                -- Priority to assign to extmarks.
+                priority = 0,
+                -- Applied to icon.
+                highlight = 'RenderMarkdownIndent',
+            },
             heading = {
                 -- Turn on / off heading icon & background rendering
                 enabled = true,
