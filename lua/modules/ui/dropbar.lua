0b5fd04d9194bcfa0c21563da35863d3b2f94ac8
Tom Deneire <tomdeneire@gmail.com>
2025-09-25 15:36:54 +0200

    (feat): nicer background for WinBar

--- lua/modules/ui/dropbar.lua
+++ lua/modules/ui/dropbar.lua
@@ -5,5 +5,7 @@ return {
         require("dropbar").setup({
             opts = { sources = { 'lsp', 'markdown', 'treesitter', 'terminal' } },
         })
+        -- nicer background for WinBar
+        vim.api.nvim_set_hl(0, "WinBar", { link = "TabLine" })
     end
 }
