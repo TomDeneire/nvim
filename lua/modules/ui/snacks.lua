099637494715975241aa22ef412f347586d7498f
Tom Deneire <tomdeneire@gmail.com>
2026-03-21 13:37:03 +0100

    (fix): ignorecase for pickers

--- lua/modules/ui/snacks.lua
+++ lua/modules/ui/snacks.lua
@@ -27,18 +27,71 @@ return {
             follow = true,
             layout = {
                 preset = "telescope",
-            }
+            },
         },
     },
     keys = {
-        { "ff",          function() Snacks.picker.git_grep({ hidden = true, untracked = true }) end,               desc = "Git grep" },
-        { "<c-f>",       function() Snacks.picker.grep({ ignored = true, hidden = true, cwd = PROJECT_DIR }) end,  desc = "Project grep" },
-        { "ts",          function() Snacks.picker.git_files({ untracked = true }) end,                             desc = "Git files" },
-        { "<c-p>",       function() Snacks.picker.files({ ignored = true, hidden = true, cwd = PROJECT_DIR }) end, desc = "Project files" },
-        { "<leader>old", function() Snacks.picker.recent({ ignored = true, hidden = true }) end,                   desc = "Find recent files" },
-        { "tb",          function() Snacks.picker.buffers() end,                                                   desc = "Show buffers" },
-        { "<leader>lg",  function() Snacks.lazygit() end,                                                          desc = "Open LazyGit" },
-        { "<leader>u",   function() Snacks.picker.undo() end,                                                      desc = "Undo tree" },
-        { "<leader>p",   function() Snacks.picker.pickers() end,                                                   desc = "Show pickers" },
+        {
+            "ff",
+            function()
+                Snacks.picker.git_grep({
+                    hidden = true,
+                    untracked = true,
+                    ignorecase = true,
+                })
+            end,
+            desc = "Git grep"
+        },
+
+        {
+            "<c-f>",
+            function()
+                Snacks.picker.grep({
+                    ignored = true,
+                    hidden = true,
+                    cwd = PROJECT_DIR,
+                    ignorecase = true,
+                })
+            end,
+            desc = "Project grep"
+        },
+
+        {
+            "ts",
+            function()
+                Snacks.picker.git_files({
+                    untracked = true,
+                })
+            end,
+            desc = "Git files"
+        },
+
+        {
+            "<c-p>",
+            function()
+                Snacks.picker.files({
+                    ignored = true,
+                    hidden = true,
+                    cwd = PROJECT_DIR,
+                })
+            end,
+            desc = "Project files"
+        },
+
+        {
+            "<leader>old",
+            function()
+                Snacks.picker.recent({
+                    ignored = true,
+                    hidden = true,
+                })
+            end,
+            desc = "Find recent files"
+        },
+
+        { "tb",         function() Snacks.picker.buffers() end, desc = "Show buffers" },
+        { "<leader>lg", function() Snacks.lazygit() end,        desc = "Open LazyGit" },
+        { "<leader>u",  function() Snacks.picker.undo() end,    desc = "Undo tree" },
+        { "<leader>p",  function() Snacks.picker.pickers() end, desc = "Show pickers" },
     },
 }
