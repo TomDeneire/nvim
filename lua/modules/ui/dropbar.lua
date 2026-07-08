35629b8dcf1381faa919555c284b0a5eb9543378
Tom Deneire <tomdeneire@gmail.com>
2026-07-08 16:07:24 +0200

    (chore): move dropbar to align with buffericon

--- lua/modules/ui/dropbar.lua
+++ lua/modules/ui/dropbar.lua
@@ -2,7 +2,13 @@ return {
     'Bekaboo/dropbar.nvim',
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-        require("dropbar").setup()
+        require("dropbar").setup({
+            bar = {
+                padding = {
+                    left = 6, -- Adjust this number to shift it further to the right
+                },
+            },
+        })
         -- nicer background for WinBar
         vim.api.nvim_set_hl(0, "WinBar", { link = "TabLine" })
     end
